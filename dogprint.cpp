b03d179e4afa6a0ff360b70375f70fd07b7e06db
nicholehayes <nhayes5@lsu.edu>
2016-09-29 20:18:34 -0500

    Add files via upload

---
+++ dogprint.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout << "   |\|\        " << endl;
+	cout << "  ..   \     . " << endl;
+	cout << "o--    \\   /@)" << endl;
+	cout << " v_///\\\\_/@  " << endl;
+	cout << "  {         }  " << endl;
+	cout << "   { } \\\{ }  " << endl;
+	cout << "   <_|    <_|  " << endl;
+	
+	cout << "woof" << endl;
+}
